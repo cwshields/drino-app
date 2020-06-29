6f07f69aff6d5b75a69d290817ef181f427a6a96
cwshields <cwshields2@gmail.com>
2020-06-29 15:33:47 -0500

    changed to BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import './assets/scss/App.scss'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import routes from './routes'
 
 export default function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       {routes}
-    </HashRouter>
+    </BrowserRouter>
   );
 }
