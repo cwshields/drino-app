bb38276b33bbdb365ecb4a320951268ba57b269c
cwshields <cwshields2@gmail.com>
2020-07-16 16:40:08 -0500

    added checkboxes & optimized code

--- src/components/Admin/Messages/MessageCard.jsx
+++ src/components/Admin/Messages/MessageCard.jsx
@@ -1,32 +1,34 @@
-import React from 'react';
-import '../../../assets/scss/messages.scss';
-import { Accordion, Card, Button } from 'react-bootstrap';
+import React from 'react'
+import '../../../assets/scss/messages.scss'
+import { Accordion, Card, Button, Form } from 'react-bootstrap'
 
-const MessageCard = (props) => {
+export default function MessageCard(props) {
   const { message } = props
-  const { first_name, last_name, email, message: userMessage } = props.message
-  
+  const { id, first_name, last_name, email, message: userMessage } = props.message
   return (
     <Card>
-      <Accordion.Toggle as={Card.Header} eventKey={message}>
-        <div className="message-header">
-          <div className="name-email">
-            <div className="name">{first_name} {last_name}</div>
-            <div className="email">{email}</div>
+      <label htmlFor={id}>
+        <Form.Check id={id} onClick={ () => props.toggleSelect(id) } type="checkbox"/>
+      </label>
+      <div className="card-wrap">
+        <Accordion.Toggle as={Card.Header} eventKey={message}>
+          <div className="message-header">
+            <div className="name-email">
+              <div className="name">{first_name} {last_name}</div>
+              <div className="email">{email}</div>
+            </div>
+            <div className="time">2:13pm 6/21/20</div>
           </div>
-          <div className="time">4:13pm 6/21/20</div>
-        </div>
-      </Accordion.Toggle>
-      <Accordion.Collapse eventKey={message}>
-        <Card.Body>{userMessage}
-          <div className="btn-wrap">
-            <Button variant="success">Reply</Button>
-            <Button variant="danger" onClick={ () => props.deleteMessage(message.id)}>Delete</Button>
-          </div>
-        </Card.Body>
-      </Accordion.Collapse>
+        </Accordion.Toggle>
+        <Accordion.Collapse eventKey={message}>
+          <Card.Body>{userMessage}
+            <div className="btn-wrap">
+              <Button href={'mailto:' + email} variant="success">Reply</Button>
+              <Button variant="danger" onClick={ () => props.deleteMessage(id) }>Delete</Button>
+            </div>
+          </Card.Body>
+        </Accordion.Collapse>
+      </div>
     </Card>
   )
 }
-
-export default MessageCard
--- src/components/Admin/Messages/Messages.jsx
+++ src/components/Admin/Messages/Messages.jsx
@@ -1,38 +1,68 @@
-import React, { Component } from 'react';
-import '../../../assets/scss/messages.scss';
-import { Accordion, Button } from 'react-bootstrap';
-import ProfileCard from '../../Profiles/ProfileCard';
+import React, { Component } from "react";
+import "../../../assets/scss/messages.scss";
+import { Accordion, Button } from "react-bootstrap";
+import ProfileCard from "../../Profiles/ProfileCard";
 // import { Editor } from 'react-draft-wysiwyg';
-import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
-import MessageCard from './MessageCard';
-import axios from 'axios';
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import MessageCard from "./MessageCard";
+import axios from "axios";
 
 export default class Messages extends Component {
   constructor() {
     super()
     this.state = {
-      messages: []
-    }
+      messages: [],
+      messagesSelected: [],
+    };
+    this.messageCard = React.createRef()
   }
 
   componentDidMount() {
     this.getMessages()
   }
-
+  
   getMessages = () => {
     axios
-      .get('/api/messages')
-      .then(res => { this.setState({ messages: res.data }) })
-      .catch(err => console.log(err))
+    .get("/api/messages")
+    .then((res) => {
+      this.setState({ messages: res.data })
+    })
+    .catch((err) => console.log(err))
+  }
+  
+  toggleSelect = (id) => {
+    const { messagesSelected } = this.state
+    const index = messagesSelected.indexOf(id)
+    if (index < 0) {
+      messagesSelected.push(id)
+    } else {
+      messagesSelected.splice(index, 1)
+    }
+    console.log(this.state.messagesSelected)
+  }
+  
+  selectAll = () => {
+    const { messages, messagesSelected } = this.state
+    for (let i = 0; i < messages.length; i++) {
+      const includes = messagesSelected.includes(messages[i].id)
+      const checkBox = document.getElementById(messages[i].id)
+      if (!includes) {
+        messagesSelected.push(messages[i].id)
+        checkBox.checked = true
+      } else {
+        this.setState({ messagesSelected: [] })
+        checkBox.checked = false
+      }
+    }
   }
 
   deleteMessage = (id) => {
     axios
       .delete(`/api/messages/${id}`)
-      .then( () => {
+      .then(() => {
         this.getMessages()
       })
-      .catch(err => console.log(err));
+      .catch((err) => console.log(err))
   }
 
   render() {
@@ -40,10 +70,12 @@ export default class Messages extends Component {
       <div className="dash-body">
         <div className="dash-section">
           <div className="messages-component-wrap">
-          <div className="count-wrap">
-            <ProfileCard />
-            <div className="count">Messages: {this.state.messages.length}</div>
-          </div>
+            <div className="count-wrap">
+              <ProfileCard />
+              <div className="count">
+                Messages: {this.state.messages.length}
+              </div>
+            </div>
             <div className="editor-messages">
               <div className="text-editor">
                 {/* 
@@ -55,22 +87,33 @@ export default class Messages extends Component {
                     onEditorStateChange={this.onEditorStateChange}
                   />
                 */}
-                <Button className="send-btn" variant="success">Send</Button>
-                <Button className="cancel-btn" variant="danger">Cancel</Button>
+                <Button className="send-btn" variant="success">
+                  Send
+                </Button>
+                <Button className="cancel-btn" variant="danger">
+                  Cancel
+                </Button>
               </div>
               <div className="messages-wrap">
                 <div className="accordion-wrap">
                   <Accordion defaultActiveKey="0">
-                    {this.state.messages.length === 0
-                      ? <h3 className='no-messages'>No messages to display...</h3>
-                      : this.state.messages.map((message, id) => (
-                        <MessageCard key={id} message={message} deleteMessage={this.deleteMessage} />
-                      ))}
+                    {this.state.messages.length === 0 
+                    ? <h3 className="no-messages">No messages to display...</h3>
+                    : this.state.messages.map((message, id) => (
+                        <MessageCard
+                          key={id}
+                          message={message}
+                          toggleSelect={this.toggleSelect}
+                          deleteMessage={this.deleteMessage}
+                        />
+                      ))
+                    }
                   </Accordion>
                 </div>
               </div>
             </div>
           </div>
+          <Button variant="success" className="select-all" onClick={this.selectAll}>Select All</Button>
         </div>
       </div>
     );
