74362d3d1e633cfb7762c40020a0756e69a2c760
cwshields <cwshields2@gmail.com>
2020-07-16 23:36:15 -0500

    improved styling

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
-import { Form, Col, Button } from 'react-bootstrap'
+import { Form, Col, Button, Jumbotron } from 'react-bootstrap'
 import '../../assets/scss/register.scss'
 import DrinoNavbar from '../Navbar/DrinoNavbar'
+import Footer from '../Navbar/Footer'
 
 export default class Register extends Component {
   render () {
@@ -10,59 +11,55 @@ export default class Register extends Component {
         <DrinoNavbar></DrinoNavbar>
         <div className="reg-wrap">
           <div className="reg-form">
-            <Form>
-              <Form.Row>
-                <Form.Group as={Col} controlId="formGridEmail">
-                  <Form.Label>Email</Form.Label>
-                  <Form.Control type="email" placeholder="Enter email" />
+            <Jumbotron>
+              <Form>
+                <Form.Row>
+                  <Form.Group as={Col} controlId="formGridEmail">
+                    <Form.Control type="email" placeholder="Enter email" />
+                  </Form.Group>
+  
+                  <Form.Group as={Col} controlId="formGridPassword">
+                    <Form.Control type="password" placeholder="Password" />
+                  </Form.Group>
+                </Form.Row>
+  
+                <Form.Group controlId="formGridAddress1">
+                  <Form.Control placeholder="1234 Main St" />
                 </Form.Group>
-
-                <Form.Group as={Col} controlId="formGridPassword">
-                  <Form.Label>Password</Form.Label>
-                  <Form.Control type="password" placeholder="Password" />
+  
+                <Form.Group controlId="formGridAddress2">
+                  <Form.Control placeholder="Apartment, studio, or floor" />
                 </Form.Group>
-              </Form.Row>
-
-              <Form.Group controlId="formGridAddress1">
-                <Form.Label>Address</Form.Label>
-                <Form.Control placeholder="1234 Main St" />
-              </Form.Group>
-
-              <Form.Group controlId="formGridAddress2">
-                <Form.Label>Address 2</Form.Label>
-                <Form.Control placeholder="Apartment, studio, or floor" />
-              </Form.Group>
-
-              <Form.Row>
-                <Form.Group as={Col} controlId="formGridCity">
-                  <Form.Label>City</Form.Label>
-                  <Form.Control />
+  
+                <Form.Row>
+                  <Form.Group as={Col} controlId="formGridCity">
+                    <Form.Control />
+                  </Form.Group>
+  
+                  <Form.Group as={Col} controlId="formGridState">
+                    <Form.Control as="select" defaultValue="Choose...">
+                      <option>Choose...</option>
+                      <option>...</option>
+                    </Form.Control>
+                  </Form.Group>
+  
+                  <Form.Group as={Col} controlId="formGridZip">
+                    <Form.Control />
+                  </Form.Group>
+                </Form.Row>
+  
+                <Form.Group id="formGridCheckbox">
+                  <Form.Check type="checkbox" label="Check me out" />
                 </Form.Group>
-
-                <Form.Group as={Col} controlId="formGridState">
-                  <Form.Label>State</Form.Label>
-                  <Form.Control as="select" defaultValue="Choose...">
-                    <option>Choose...</option>
-                    <option>...</option>
-                  </Form.Control>
-                </Form.Group>
-
-                <Form.Group as={Col} controlId="formGridZip">
-                  <Form.Label>Zip</Form.Label>
-                  <Form.Control />
-                </Form.Group>
-              </Form.Row>
-
-              <Form.Group id="formGridCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
-              </Form.Group>
-
-              <Button variant="primary" type="submit">
-                Submit
-              </Button>
-            </Form>
+  
+                <Button variant="primary" type="submit">
+                  Submit
+                </Button>
+              </Form>
+            </Jumbotron>
           </div>
         </div>
+        <Footer></Footer>
       </div>
     )
   }
