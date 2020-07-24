36f7136aa7ca682067840c473885ea0f846fc6d7
cwshields <cwshields2@gmail.com>
2020-07-23 23:09:19 -0500

    improved styling

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "../../assets/scss/contact.scss";
+import Footer from "../Navbar/Footer";
 
 // import { connect, sendMessage } from "react-redux";
 import axios from "axios";
@@ -52,78 +53,86 @@ class Contact extends Component {
   render() {
     const { firstName, lastName, email, message, isSubmittable } = this.state;
     return (
-      <div className="contact-page">
+      <div className="drino">
         <DrinoNavbar></DrinoNavbar>
-        <div className="contact-wrap">
-          <h1>Contact</h1>
-          <Jumbotron>
-            <Form className="form">
-              <Form.Row>
-                <Col>
-                  <Form.Control
-                    required
-                    onChange={this.handleChange}
-                    value={firstName}
-                    name="firstName"
-                    className="form-input"
-                    type="text"
-                    placeholder="First name"
-                  />
-                </Col>
-                <Col>
-                  <Form.Control
-                    required
-                    onChange={this.handleChange}
-                    value={lastName}
-                    name="lastName"
-                    type="text"
-                    className="form-input"
-                    placeholder="Last name"
-                  />
-                </Col>
-              </Form.Row>
-              <Form.Group>
-                <Form.Control
-                  required
-                  onChange={this.handleChange}
-                  value={email}
-                  name="email"
-                  className="form-input"
-                  type="text"
-                  placeholder="email@example.com"
-                />
-              </Form.Group>
-              <Form.Group>
-                <Form.Control
-                  required
-                  onChange={this.handleChange}
-                  value={message}
-                  name="message"
-                  className="form-text"
-                  type="text"
-                  as="textarea"
-                  rows="10"
-                  placeholder="Write message here..."
-                />
-                <div className="minimum">Min Length: 50</div>
-                <div
-                  className={
-                    isSubmittable ? "message-length" : "message-length red-text"
-                  }
-                >
-                  {message.length}/500
-                </div>
-              </Form.Group>
-            </Form>
-            <Button className=/*{this.props.login ? */"devlog"/* : "display-none"}*/ onClick={this.devlog}>Devlog</Button>
-            <Button
-              className={isSubmittable ? "submit-btn" : "submit-btn disabled"}
-              onClick={isSubmittable ? this.postMessage : null}
-            >
-              Send Message
-            </Button>
-          </Jumbotron>
-        </div>
+        <main>
+          <div className="contact-container">
+            <div className="contact-wrap">
+              <div className="contact-form drino-form">
+                <h1>Contact</h1>
+                <Jumbotron>
+                  <Form className="form">
+                    <Form.Row>
+                      <Form.Group as={Col}>
+                        <Form.Control
+                          required
+                          onChange={this.handleChange}
+                          value={firstName}
+                          name="firstName"
+                          className="form-input form-required"
+                          type="text"
+                          placeholder="First name"
+                        />
+                      </Form.Group>
+                      <Form.Group as={Col}>
+                        <Form.Control
+                          required
+                          onChange={this.handleChange}
+                          value={lastName}
+                          name="lastName"
+                          type="text"
+                          className="form-input form-required"
+                          placeholder="Last name"
+                        />
+                      </Form.Group>
+                    </Form.Row>
+                    <Form.Group>
+                      <Form.Control
+                        required
+                        onChange={this.handleChange}
+                        value={email}
+                        name="email"
+                        className="form-input form-required"
+                        type="text"
+                        placeholder="email@example.com"
+                      />
+                    </Form.Group>
+                    <Form.Group>
+                      <Form.Control
+                        required
+                        onChange={this.handleChange}
+                        value={message}
+                        name="message"
+                        className="form-text form-required"
+                        type="text"
+                        as="textarea"
+                        rows="10"
+                        placeholder="Write message here..."
+                      />
+                      <div className="minimum">Min Length: 50</div>
+                      <div
+                        className={
+                          isSubmittable ? "message-length" : "message-length red-text"
+                        }
+                      >
+                        {message.length}/500
+                      </div>
+                    </Form.Group>
+                  </Form>
+                  <Button className=/*{this.props.login ? */"devlog"/* : "display-none"}*/ onClick={this.devlog}>Devlog</Button>
+                  <Button
+                    className={isSubmittable ? "submit-btn" : "submit-btn disabled"}
+                    onClick={isSubmittable ? this.postMessage : null}
+                  >
+                    Send Message
+                  </Button>
+                  <div className="required">Required</div>
+                </Jumbotron>
+              </div>
+            </div>
+            <Footer></Footer>
+          </div>
+        </main>
       </div>
     );
   }
--- src/components/Navbar/Footer.jsx
+++ src/components/Navbar/Footer.jsx
@@ -5,43 +5,46 @@ import { Button } from "react-bootstrap";
 const Footer = () => {
   return (
     <footer>
-      <div className="subscribe-wrap">
-        <div className="text-wrap">
-          <div className="label">Let's Stay In Touch</div>
-          <div className="subtext">
-            Subscribe for updates, special offers and more
+      <div className="footer-wrap">
+        <div className="subscribe-wrap">
+          <div className="text-wrap">
+            <div className="label">Let's Stay In Touch</div>
+            <div className="subtext">
+              Subscribe for updates, special offers and more
+            </div>
+          </div>
+          <div className="form">
+            <input
+              aria-labelledby="email-signup"
+              placeholder="email@brand.com"
+              type="text"
+              name="email"
+              id="email"
+            />
+            <Button id="email-signup" className="sign-up-btn" variant="success">Sign Up</Button>
           </div>
         </div>
-        <div className="form">
-          <input
-            placeholder="email@brand.com"
-            type="text"
-            name="email"
-            id="email"
-          />
-          <Button className="sign-up-btn" variant="success">Sign Up</Button>
+        <div className="sm-btn-wrap">
+          <Button aria-label="facebook" className="sm-btn" variant="success">
+            <i className="fab fa-facebook-f"></i>
+          </Button>
+          <Button aria-label="twitter" className="sm-btn" variant="success">
+            <i className="fab fa-twitter"></i>
+          </Button>
+          <Button aria-label="linkedin" className="sm-btn" variant="success">
+            <i className="fab fa-linkedin-in"></i>
+          </Button>
         </div>
-      </div>
-      <div className="sm-btn-wrap">
-        <Button className="sm-btn" variant="success">
-          <i className="fab fa-facebook-f"></i>
-        </Button>
-        <Button className="sm-btn" variant="success">
-          <i className="fab fa-twitter"></i>
-        </Button>
-        <Button className="sm-btn" variant="success">
-          <i className="fab fa-linkedin-in"></i>
-        </Button>
-      </div>
-      <div className="support">
-        <div>Do you have a question? Send us a message:</div>
-        <a href="mailto:cwshields2@gmail.com">
-          <div className="green email">support@drino.live</div>
-        </a>
-      </div>
-      <div className="copyright">
-        Copyright &copy; {new Date().getFullYear()} All rights reserved | Drino
-        Technologies Inc.
+        <div className="support">
+          <div>Do you have a question? Send us a message:</div>
+          <a href="mailto:cwshields2@gmail.com">
+            <div className="green email">support@drino.live</div>
+          </a>
+        </div>
+        <div className="copyright">
+          Copyright &copy; {new Date().getFullYear()} All rights reserved | Drino
+          Technologies Inc.
+        </div>
       </div>
     </footer>
   );
